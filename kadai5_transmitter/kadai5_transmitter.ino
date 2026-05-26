a451cc2e276ed5652730b309ed077b54ebbaf9d3
shirokuma89dev <code@shirokuma89.dev>
2026-05-26 11:25:15 +0900

    ✅ Kadai5

---
+++ kadai5_transmitter/kadai5_transmitter.ino
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+
+int outpin = 8;
+byte data = B10;
+int dly = 100;
+int dly2 = 500;
+
+const char* msg = "02246909";
+
+void setup() {
+    pinMode(outpin, OUTPUT);
+    digitalWrite(outpin, HIGH);
+}
+
+void loop() {
+    digitalWrite(outpin, 0);  // スタートビット（0）の送信
+    delay(dly);
+    for (int index = 0; index < 8; index++) {
+        {
+            for (int i = 0; i < 7; i++) {
+                digitalWrite(outpin,
+                             bitRead(msg[index],
+                                     i));  // msg の右からi 番目のビットを送信
+                delay(dly);
+            }
+        }
+        digitalWrite(outpin, 1);  // ストップビット（1）の送信
+        delay(dly2);
+    }
+}
